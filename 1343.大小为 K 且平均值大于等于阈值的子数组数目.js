3c094f6b0ef3730b3a3b73b11f4052994d88676b
碎碎酱 <yinxin630@gmail.com>
2020-02-19 14:04:53 +0800

    Create 1343.大小为 K 且平均值大于等于阈值的子数组数目.js

---
+++ 1343.大小为 K 且平均值大于等于阈值的子数组数目.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} threshold
+ * @return {number}
+ */
+var numOfSubarrays = function(arr, k, threshold) {
+    let result = 0;
+    const target = threshold * k;
+    
+    const sum = [arr[0]];
+    for (let i = 1; i < arr.length; i++) {
+        sum[i] = sum[i - 1] + arr[i];
+    }
+
+    for (let i = k - 1; i < arr.length; i++) {
+        if (sum[i] - (sum[i - k] || 0) >= target) {
+            result++;
+        }
+    }
+
+    return result;
+};
+
+console.log(numOfSubarrays(arr = [7,7,7,7,7,7,7], k = 7, threshold = 7));
