f1ac02a0fb88e24a57333626fbc201b6d2e2d5a3
碎碎酱 <yinxin630@gmail.com>
2020-02-26 17:48:09 +0800

    Create 16.最接近的三数之和.js

---
+++ 16.最接近的三数之和.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b);
+
+    let result = Number.MAX_SAFE_INTEGER;
+    for (let i = 0; i < nums.length - 2; i++) {
+        let a = i + 1;
+        let b = nums.length - 1;
+        while (a < b) {
+            const sum = nums[a] + nums[b] + nums[i];
+            if (Math.abs(target - sum) < Math.abs(target - result)) {
+                result = sum;
+            }
+            if (sum < target) {
+                a++;
+            } else if (sum > target) {
+                b--;
+            } else {
+                return target;
+            }
+        }
+    }
+
+    return result;
+};
+
+console.log(threeSumClosest([-1,2,1,-4], 1));
