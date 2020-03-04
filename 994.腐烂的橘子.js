fdc3aa6eaaa7b0e6ac55f7262fe6c2914b59ecb9
碎碎酱 <yinxin630@gmail.com>
2020-03-04 11:22:11 +0800

    Create 994.腐烂的橘子.js

---
+++ 994.腐烂的橘子.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function(grid) {
+    let good = 0;
+    let bad = [];
+    let count = 0;
+    const direction = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 1) {
+                good++;
+            } else if (grid[i][j] === 2) {
+                bad.push({i, j});
+            }
+        }
+    }
+
+    if (good === 0) {
+        return 0;
+    }
+
+    while (bad.length) {
+        count++;
+        const queue = bad;
+        bad = [];
+
+        while (queue.length) {
+            const {i, j} = queue.pop();
+            for (const [di, dj] of direction) {
+                const nexti = i + di;
+                const nextj = j + dj;
+                if (grid[nexti] && grid[nexti][nextj] === 1) {
+                    good--;
+                    grid[nexti][nextj] = 2;
+                    bad.push({i: nexti, j: nextj});
+                }
+            }
+        }
+    }
+
+    return good === 0 ? count - 1 : -1;
+};
+
+console.log(orangesRotting([[2,1,1],[1,1,0],[0,1,1]]));
