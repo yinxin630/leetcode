f3c0788781dee86edb3cb7289ebee054ea5bea56
碎碎酱 <yinxin630@gmail.com>
2020-03-09 20:45:20 +0800

    Create 47.全排列 II.js

---
+++ 47.全排列 II.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+    const temp = [];
+    
+    function dfs() {
+        if (nums.length === 0) {
+            result.push(temp.slice());
+            return;
+        }
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] !== nums[i - 1]) {
+                temp.push(nums[i]);
+                nums.splice(i, 1);
+                dfs();
+                const num = temp.pop();
+                nums.splice(i, 0, num);
+            }
+        }
+    }
+    dfs();
+
+    return result;
+};
+
+console.log(permuteUnique([1, 3, 2]));
