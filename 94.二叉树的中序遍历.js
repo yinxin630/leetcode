ca18132b0ea8934cab40199bbeef1973de476b87
碎碎酱 <yinxin630@gmail.com>
2020-03-11 00:54:02 +0800

    Create 94.二叉树的中序遍历.js

---
+++ 94.二叉树的中序遍历.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const result = [];
+    const stack = [];
+    let cur = root;
+    while (cur !== null || stack.length !== 0) {
+        while (cur !== null) {
+            stack.push(cur);
+            cur = cur.left;
+        }
+        cur = stack.pop();
+        result.push(cur.val);
+        cur = cur.right;
+    }
+    return result;
+};
