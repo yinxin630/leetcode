8132c03feea4e24da5bb5a884ff200880db506de
碎碎酱 <yinxin630@gmail.com>
2020-05-08 20:44:42 +0800

    Create 1417.重新格式化字符串.js

---
+++ 1417.重新格式化字符串.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reformat = function(s) {
+    const char = [];
+    const number = [];
+    for (const a of s) {
+        if (isNaN(+a)) {
+            char.push(a);
+        } else {
+            number.push(a);
+        }
+    }
+    if (Math.abs(char.length - number.length) > 1) {
+        return '';
+    }
+    const result = [];
+    const max = char.length >= number.length ? char : number;
+    for (let i = 0; i < max.length; i++) {
+        result[i * 2] = max[i];
+    }
+    const min = char === max ? number : char;
+    for (let i = 0; i < min.length; i++) {
+        result[i * 2 + 1] = min[i];
+    }
+    return result.join('');
+};
+
+console.log(reformat("ab123"));
