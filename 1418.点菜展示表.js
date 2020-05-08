64a0cbf97c988e575764c39684e1678ac1ae5365
碎碎酱 <yinxin630@gmail.com>
2020-05-08 20:44:44 +0800

    Create 1418.点菜展示表.js

---
+++ 1418.点菜展示表.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[][]} orders
+ * @return {string[][]}
+ */
+var displayTable = function(orders) {
+    const foods = new Set();
+    const table = new Map();
+    for (const order of orders) {
+        foods.add(order[2]);
+        const m = table.get(order[1]) || new Map();
+        const c = m.get(order[2]) || 0;
+        m.set(order[2], c + 1);
+        table.set(order[1], m);
+    }
+
+    const result = [['Table']];
+    result[0].push(...Array.from(foods).sort((a, b) => a < b ? -1 : 1));
+    // console.log(result);
+
+    const tables = Array.from(table.keys()).sort((a, b) => +a - +b);
+    // console.log(tables);
+    for (const t of tables) {
+        const a = [t];
+        const m = table.get(t);
+        for (let i = 1; i < result[0].length; i++) {
+            a.push((m.get(result[0][i]) || 0).toString());
+        }
+        result.push(a);
+    }
+    return result;
+};
+
+console.log(displayTable(orders = [["Laura","2","Bean Burrito"],["Jhon","2","Beef Burrito"],["Melissa","2","Soda"]]));
