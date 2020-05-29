43188a1938e32940df675476883ce8a98cfc3679
碎碎酱 <yinxin630@gmail.com>
2020-05-29 22:54:32 +0800

    Create 1456.定长子串中元音的最大数目.js

---
+++ 1456.定长子串中元音的最大数目.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function(s, k) {
+    const arr = new Array(s.length);
+    arr[-1] = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'a' || s[i] === 'e' || s[i] === 'i' || s[i] === 'o' || s[i] === 'u') {
+            arr[i] = arr[i - 1] + 1;
+        } else {
+            arr[i] = arr[i - 1];
+        }
+    }
+    let result = arr[k - 1];
+    for (let i = k; i < arr.length; i++) {
+        result = Math.max(result, arr[i] - arr[i - k]);
+    }
+    return result;
+};
+
+console.log(maxVowels(s = "tryhard", k = 4));
