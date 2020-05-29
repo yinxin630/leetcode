3408da8dc7538de6f7dd6775616ed0708275c5bd
碎碎酱 <yinxin630@gmail.com>
2020-05-29 22:54:29 +0800

    Create 1455.检查单词是否为句中其他单词的前缀.js

---
+++ 1455.检查单词是否为句中其他单词的前缀.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} sentence
+ * @param {string} searchWord
+ * @return {number}
+ */
+var isPrefixOfWord = function(sentence, searchWord) {
+    const i =  sentence.split(' ').findIndex(x => x.startsWith(searchWord));
+    return i === -1 ? -1 : i + 1;
+};
+
+console.log(isPrefixOfWord(sentence = "i am tired", searchWord = "you"));
