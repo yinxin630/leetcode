c5e0e20dce2aa2e375eb2935c371c56aa8d4cda7
碎碎酱 <yinxin630@gmail.com>
2020-06-05 13:45:51 +0800

    Create 54.螺旋矩阵.js

---
+++ 54.螺旋矩阵.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    if (matrix.length < 1) {
+        return [];
+    }
+    const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    let d = 0;
+    let x = 0, y = -1;
+    let count = matrix.length * matrix[0].length;
+    let result = [];
+    while (count) {
+        const nextX = x + directions[d][0];
+        const nextY = y + directions[d][1];
+        if (matrix[nextX] && matrix[nextX][nextY] !== undefined) {
+            result.push(matrix[nextX][nextY]);
+            matrix[nextX][nextY] = undefined;
+            count--;
+            x = nextX;
+            y = nextY;
+        } else {
+            d = (d + 1) % directions.length;
+        }
+    }
+    return result;
+};
+
+console.log(spiralOrder(
+    [
+        [1, 2]
+      ]
+));
