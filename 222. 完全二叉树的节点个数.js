a690e20ac389fd633d9b1d5f80e96a99027c7e90
碎碎酱 <yinxin630@gmail.com>
2020-12-02 21:49:28 +0800

    Update 222. 完全二叉树的节点个数.js

--- 222. 完全二叉树的节点个数.js
+++ 222. 完全二叉树的节点个数.js
@@ -9,12 +9,41 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var countNodes = function(root) {
-  function dfs(node) {
-    if (!node) {
-      return 0;
+var countNodes = function (root) {
+  function isExist(level, k) {
+    let bits = 1 << (level - 1);
+    let node = root;
+    while (node && bits) {
+      if (k & bits) {
+        node = node.right;
+      } else {
+        node = node.left;
+      }
+      bits = bits >> 1;
     }
-    return 1 + dfs(node.left) + dfs(node.right);
+    return node;
   }
-  return dfs(root);
+
+  if (!root) {
+    return 0;
+  }
+
+  let level = 0;
+  let node = root;
+  while (node.left) {
+    level++;
+    node = node.left;
+  }
+
+  let l = 1 << level;
+  let r = (1 << (level + 1)) - 1;
+  while (l < r) {
+    const mid = Math.ceil((l + r) / 2);
+    if (isExist(level, mid)) {
+      l = mid;
+    } else {
+      r = mid - 1;
+    }
+  }
+  return l;
 };
