88f7ddfa131938e939a2b497ad2098902e01771c
碎碎酱 <yinxin630@gmail.com>
2020-12-07 21:15:43 +0800

    Update 1679. K 和数对的最大数目.js

--- 1679. K 和数对的最大数目.js
+++ 1679. K 和数对的最大数目.js
@@ -5,38 +5,20 @@
  */
 var maxOperations = function (nums, k) {
   nums.sort((a, b) => a - b);
-  function binarySearch(i, j, v) {
-    while (i <= j) {
-      const mid = Math.ceil((i + j) / 2);
-      if (nums[mid] === v) {
-        return mid;
-      } else if (nums[mid] < v) {
-        i = mid + 1;
-      } else {
-        j = mid - 1;
-      }
-    }
-    return -1;
-  }
 
   let result = 0;
   let i = 0;
-  while (nums.length > 1 && i < nums.length - 1) {
-    if (nums[i] >= k) {
-      break;
+  let j = nums.length - 1;
+  while (i < j) {
+    if (nums[i] + nums[j] === k) {
+      i++;
+      j--;
+      result++;
+    } else if (nums[i] + nums[j] < k) {
+      i++;
+    } else {
+      j--;
     }
-
-    if (k - nums[i] >= nums[i]) {
-      const target = binarySearch(i + 1, nums.length - 1, k - nums[i]);
-      if (target !== -1) {
-        nums.splice(target, 1);
-        nums.splice(i, 1);
-        result++;
-        continue;
-      }
-    }
-    i++;
   }
-
   return result;
 };
